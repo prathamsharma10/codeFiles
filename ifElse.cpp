4c3e164d8c41bd68ca6ea5519828c6a4d124f96d
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-01-03 17:49:35 +0530

    Add files via upload

---
+++ ifElse.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //if-block
+    // int balance = 10;
+    // if(balance>=10){
+    //     // cout<<"Maggi";
+    // }
+
+    //if-elseblock
+    // int balance;
+    // cout<<"Enter balance: ";
+    // cin>>balance;
+    // if(balance>10){
+    //     cout<<"Maggi";
+    // }
+    // else{
+    //     cout<<"Kurkure";
+    // }
+
+    // int sides;
+    // cout<<"Enter sides: ";
+    // cin>>sides;
+    // if(sides==3){
+    //     cout<<"Triangle";
+    // }
+    // else{
+    //     cout<<"Not a Triangle";
+    // }
+
+    //if-else if-if block
+    // int sides;
+    // cout<<"Enter sides: ";
+    // cin>>sides;
+    // if(sides==3){
+    //     cout<<"Triangle";
+    // }
+    // else if(sides==4){
+    //     cout<<"Sqaure";
+    // }
+    // else if(sides==5)
+    // {
+    //     cout<<"Pentagon";
+    // }
+    // else{
+    //     cout<<"Other Shape";
+    // }
+
+    // int num;
+    // cout<<"Enter Number: ";
+    // cin>>num;
+    // if(num > 0){
+    //     cout<<"Positive";
+    // }
+    // else if(num < 0){
+    //     cout<<"Negative";
+    // }
+    // else{
+    //     cout<<"Zero";
+    // }
+
+    // even-odd
+    int num;
+    cout<<"Enter number: ";
+    cin>>num;
+    if(num%2==0){
+        cout<<"Even number";
+    }
+    else{
+        cout<<"Odd Number";
+    }
+
+
+    return 0;
+}
---
+++ loops.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //for loop
+    // for(int i=10;i<=12; i=i+1){
+    //     cout<<"Monica My Darling"<<endl;
+    // }
+
+    // for(int i=1; i<=10; i=i+1){
+    //     cout<<i<<" ";
+    // }
+
+    // for(int i=10; i>=0; i = i-1){
+    //     cout<<i<<" ";
+    // }
+
+    for(int i=0;i<5;i++){
+        cout<<"A"<<" ";
+    }
+    return 0;
+}
