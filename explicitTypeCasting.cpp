2ad657eaf1b587a9f6b8009f424fda2a491b7c00
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-01-05 18:58:11 +0530

    Add files via upload

---
+++ explicitTypeCasting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1 =10;
+    float num2 = 6.8;
+    float result = num1 + (int)num2;
+    cout<<result; 
+
+    return 0;
+}
---
+++ implicitTypeCasting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int num = 10;
+    // float point = 15.5;
+    // float result;
+    // result = num + point;
+    // cout<<result;
+
+    char ch = 'A'; //65
+    int a = ch + 1 ; //66=>B
+    
+    cout<<a;
+
+    return 0;
+}
