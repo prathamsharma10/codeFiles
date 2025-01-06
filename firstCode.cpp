5e4c46bfda0d8e74ecb19837e5c1cd10e93356c7
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-01-06 18:32:53 +0530

    Add files via upload

---
+++ firstCode.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+// code starts from main
+int main(){
+    // cout << "Pratham Sharma"<<endl;
+    // cout<<'\n';
+    cout << "Pratham Sharma";  cout<<"Pratham Sharma";
+    // cout<< "Pratham" <<endl << "Sharma" <<'\n';
+}
---
+++ inputOutput.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // //int -> 4byte
+    // int num = 50;
+    // cout << num <<endl;
+
+    // cout << sizeof(num) <<endl;      
+
+    // //char -> 1byte
+
+    // char ch = 'a';
+    // cout<< ch <<endl;
+    // cout<< sizeof(ch) <<endl; 
+
+    // //float -> 4byte
+    // float point = 1.23;
+    // cout<< point << endl;
+    // cout << sizeof(point) <<endl;
+
+    // //long -> 4byte or 8byte  compiler dependent 
+    // long value = 1345;
+    // cout<< value <<endl; 
+    // cout << sizeof(value) <<endl;   
+
+    //Garbage value
+    // int newValue;
+    // cout<< newValue <<endl;
+
+    // Input => cin
+
+    // int marks;
+    // cout<<"Enter marks : ";
+    // //take input from user
+    // cin>>marks;
+    // cout<<"Marks is: "<<marks;
+
+    char ch;
+    cout<<"Enter character: ";
+    cin>>ch;
+    cout<<"Character is: "<<ch<<endl;
+    return 0;
+}
